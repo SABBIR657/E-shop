287c2948e1c8dee213809497ece155e20c94ef0d
SABBIR657 <mdsabbirrahman2025@gmail.com>
2025-02-20 09:37:53 +0600

    ready for deploy

---
+++ src/pages/Selling.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SellingMade from '../components/SellingMade/SellingMade';
+import NewsletterSection from '../components/closetProducts/NewsletterSection';
+
+const Selling = () => {
+  return (
+    <div className='bg-white'>
+      <SellingMade />
+      <NewsletterSection />
+    </div>
+  );
+};
+
+export default Selling;
